fcc328f9b30a5b49ca4d422206b016f1d27de9e6
Charly Escalona <charly2221@hotmail.fr>
2023-02-28 14:09:18 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import * as React from 'react';
 import {
     MD3LightTheme as DefaultTheme,
@@ -29,8 +8,7 @@ import {createStore} from "redux";
 const theme = {
     ...DefaultTheme,
 };
-import rootReducer from './reducers'
-const store = createStore(rootReducer)
+
 
 export default function App() {
     return (
